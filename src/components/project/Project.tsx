d0aac0d946d2da278bd2ec5d752c7ca18fa451bf
Yoeri Otten <yoeori@live.nl>
2020-10-05 21:49:46 +0200

    Work on project view to be able to add multiple files

--- src/components/project/Project.tsx
+++ src/components/project/Project.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
-import { Box, IconButton } from '@material-ui/core';
+import { Box, Button, Dialog, DialogContent, DialogTitle, Drawer, IconButton, List, ListItem, ListItemText, ListSubheader } from '@material-ui/core';
 import PictureAsPdfIcon from '@material-ui/icons/PictureAsPdf';
 import HomeIcon from '@material-ui/icons/Home';
 import InfoIcon from '@material-ui/icons/Info';
-import { EditorState } from 'draft-js';
+import MenuIcon from '@material-ui/icons/Menu';
 
 import EmptyProject from './EmptyProject';
 import DocumentView from './DocumentView';
@@ -25,42 +25,71 @@ export default function Project() {
         [name: string]: { // Probably later will be some kind of uuid
             name: string,
             pdf: Int8Array,
-            editor: EditorState
+            documentId: string
         }
     }>({});
 
     const [currentFile, setCurrentFile] = useState<string>(null);
 
     const addProjectFile = (name: string, file: Int8Array) => {
-        setFiles((cur) => { return { ...cur, [name]: { name, pdf: file, editor: EditorState.createEmpty() }}; });
+        setFiles((cur) => { return { ...cur, [name]: { name, pdf: file, documentId: name }}; });
 
         if (!currentFile) {
             setCurrentFile(name);
         }
     };
 
-    const updateEditorState = (name: string, newState: EditorState) => {
-        setFiles((cur) => { return { ...cur, [name]: { ...cur[name], editor: newState } }; });
-    };
+    const [isFileDrawerOpen, setFileDrawerOpen] = useState(false);
+    const [isAddFilesModalOpen, setAddFilesModalOpen] = useState(false);
 
     return (
-        <Box flexDirection="column" display="flex" height="100%">
-            <Box m={0} bgcolor="primary.main">
-                <IconButton href="/"><HomeIcon style={{color: '#fff'}} fontSize="small"/></IconButton >
-                <IconButton href="/about"><InfoIcon style={{color: '#fff'}} fontSize="small"/></IconButton>
-                <IconButton href="/project"><PictureAsPdfIcon style={{color: '#fff'}} fontSize="small"/></IconButton>
-            </Box>
+        <>
+            <Drawer anchor="left" open={isFileDrawerOpen} onClose={() => setFileDrawerOpen(false)}>
+                <List component="nav" aria-labelledby="nested-list-subheader"
+                    subheader={
+                        <ListSubheader component="div" id="nested-list-subheader">
+                            Project files
+                        </ListSubheader>
+                    } style={{ minWidth: '300px' }} >
 
-            <Box flexGrow={1} style={{
-                minHeight: '0'
-            }}>
-                {Object.keys(files).length !== 0 && currentFile ? <>
-                    <DocumentView pdf={{ data: files[currentFile].pdf }} editorState={files[currentFile].editor}
-                                  updateEditorState={(newState) => updateEditorState(currentFile, newState)} />
-                </> : <>
+                    {Object.keys(files).map((name, id) => {
+                        return (<ListItem key={id} button onClick={() => {
+                            console.log(name);
+                            setCurrentFile(name);
+                        }}>
+                            <ListItemText primary={name} />
+                        </ListItem>);
+                    })}
+                </List>
+                <Button onClick={() => setAddFilesModalOpen(true)}>Add new files</Button>
+            </Drawer>
+
+            <Dialog onClose={() => setAddFilesModalOpen(false)} aria-labelledby="customized-dialog-title" open={isAddFilesModalOpen}>
+                <DialogTitle>Add files to your project</DialogTitle>
+                <DialogContent dividers>
                     <EmptyProject addFile={addProjectFile} />
-                </>}
+                </DialogContent>
+            </Dialog>
+
+            <Box flexDirection="column" display="flex" height="100%">
+                <Box m={0} bgcolor="primary.main">
+                    <IconButton onClick={() => setFileDrawerOpen(true)}><MenuIcon style={{ color: '#fff' }}/></IconButton>
+
+                    <IconButton href="/"><HomeIcon style={{color: '#fff'}} fontSize="small"/></IconButton >
+                    <IconButton href="/about"><InfoIcon style={{color: '#fff'}} fontSize="small"/></IconButton>
+                    <IconButton href="/project"><PictureAsPdfIcon style={{color: '#fff'}} fontSize="small"/></IconButton>
+                </Box>
+
+                <Box flexGrow={1} style={{
+                    minHeight: '0'
+                }}>
+                    {Object.keys(files).length !== 0 && currentFile ? <>
+                        <DocumentView pdf={{ data: files[currentFile].pdf }} documentId={files[currentFile].documentId} />
+                    </> : <>
+                        <EmptyProject addFile={addProjectFile} />
+                    </>}
+                </Box>
             </Box>
-        </Box>
+        </>
     );
 }
