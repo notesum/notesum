ace658fab614502aa73b03f962a8e3234459bb99
Yoeri Otten <yoeori@live.nl>
2020-10-07 21:59:47 +0200

    Add small error page

---
+++ src/components/Error.tsx
@@ -0,0 +1,12 @@
+import { Container } from '@material-ui/core';
+import React from 'react';
+
+export default React.memo(() => {
+    // TODO style this page.
+
+    return (
+        <Container fixed>
+            <p>Sorry, the page you were looking for couldn't be found</p>
+        </Container>
+    );
+});
