64d33e425071c9bd730b08f3448e68910cd185ac
Yoeri Otten <yoeori@live.nl>
2020-10-07 18:06:04 +0200

    Add redux store for projects (not yet one for related files)

--- src/redux/reducers/index.tsx
+++ src/redux/reducers/index.tsx
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 
 import pdfReducer from './pdfReducer';
 import editorReducer from './editorReducer';
+import projectsReducer from './projectsReducer';
 
 const rootReducer = combineReducers({
     editor: editorReducer,
     pdf: pdfReducer,
+    projects: projectsReducer
 });
 
 export type AppState = ReturnType<typeof rootReducer>;
