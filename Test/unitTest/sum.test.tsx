aeaa00e8c65cbff0a39a063dd7c0bf79595a8ce8
emulateCozener <emulatejunky@gmail.com>
2020-10-16 01:31:15 +0200

    Added test for login and documentation for selenium

--- Test/unitTest/sum.test.tsx
+++ Test/unitTest/sum.test.tsx
@@ -3,6 +3,6 @@ import {expect, test} from '@jest/globals';
 // tslint:disable-next-line:no-var-requires
 const sum = require('./sum');
 
-test('adds 1 + 2 to equal 3', () => {
+test('Always true 1=2=3', () => {
     expect(sum(1, 2)).toBe(3);
 });
