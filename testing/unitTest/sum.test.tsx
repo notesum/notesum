467e498f94fb6884b651bcad31dc22549acadde1
emulateCozener <emulatejunky@gmail.com>
2020-10-20 01:59:58 +0200

    deleted things

--- testing/unitTest/sum.test.tsx
+++
@@ -1,8 +0,0 @@
-import {expect, test} from '@jest/globals';
-
-// tslint:disable-next-line:no-var-requires
-const sum = require('./sum');
-
-test('Always true 1=2=3', () => {
-    expect(sum(1, 2)).toBe(3);
-});
