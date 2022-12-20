eb2573f42fb9aede47ace925c61269cc87330812
Francesco La Camera <fm@lacamera.org>
2022-12-21 00:12:07 +0100

    src/components/editor: add support for md/txt output

--- src/components/editor/Download.tsx
+++ src/components/editor/Download.tsx
@@ -1,29 +1,44 @@
 import { convertToRaw, EditorState } from 'draft-js';
 import draftToHtml from 'draftjs-to-html';
+import { draftToMarkdown } from 'markdown-draft-js';
 import { saveAs } from 'file-saver';
 
 import generateWordDoc from './Word';
 import pdfDownload from './PdfDownload';
 
-/**
- * Download an EditorState as a file
- * @param eState EditorState to be downloaded
- * @param extension The file extension to be used, so file type to be downloaded
- * @param name Name of the file to be downloaded
- */
-export default function downloadState(eState: EditorState, extension: string, name: string): void {
-    if (extension === 'html' || extension === 'txt') { htmlOrtxt(eState, extension, name); }
-    else if (extension === 'docx') {generateWordDoc(eState.getCurrentContent(), name);}
-    else if (extension === 'pdf') { pdfDownload(eState.getCurrentContent(), name); }
-
+function draftToText(draft: RawDraftContentState): string {
+  return draft.blocks
+    .map(b => (!b.text.trim() && '\n') || b.text)
+    .join('\n')
 }
 
-// TODO: handle images
-// Handle .txt or .html coversion and download
-function htmlOrtxt(eState: EditorState, ex: 'html' | 'txt', name: string) {
-
-    const contents = convertToRaw(eState.getCurrentContent());
-    const markup = draftToHtml(contents, true);
-    const file = new Blob([markup.toString()]);
-    saveAs(file, name + '.' + ex);
+/**
+ * Download an EditorState as a file
+ * @param s EditorState
+ * @param ext The file extension to be used, so file type to be downloaded
+ * @param name Name of the file to be downloaded
+ */
+export default function downloadState(s: EditorState, ext: string, name: string): void {
+    const draft = s.getCurrentContent();
+    const fn = (name + '.' + ext)
+    let fmt, blob
+    switch (ext) {
+      case "md":
+        fmt = draftToMarkdown(convertToRaw(draft));
+        blob = new Blob([fmt]);
+        saveAs(blob, fn);
+      break;
+      case "txt":
+        fmt = draftToText(convertToRaw(draft));
+        blob = new Blob([fmt]);
+        saveAs(blob, fn);
+        break;
+      case "html":
+        fmt = draftToHtml(convertToRaw(draft), true);
+        blob = new Blob([fmt]);
+        saveAs(blob, fn);
+        break;
+      case "docx": generateWordDoc(draft, name); break;
+      case "pdf": pdfDownload(draft, name); break;
+    }
 }
